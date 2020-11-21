d8f8d01b8220fe1cccd84c615a56f8c59a62f22e
Jason Fomuke <jfomuke@uncc.edu>
2020-11-20 20:21:30 -0500

    Updates

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+
+module.exports = 
+{
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    singleQuote: true,
+    trailingComma: "all",
+    bracketSpacing: true
+  };
